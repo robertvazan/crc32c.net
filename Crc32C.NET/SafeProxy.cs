78fee1e8d152a793cb31e61332d96ead4049005c
force <force@force-net.com>
2016-06-19 15:46:11 +0300

    improves to hash calc speed up to 150MB/s due better .NET object handling

--- Crc32C.NET/SafeProxy.cs
+++ Crc32C.NET/SafeProxy.cs
@@ -1,7 +1,7 @@
 ﻿/* This is .NET safe implementation of Crc32C algorithm.
  * This implementation was found fastest from some variants, based on Robert Vazan native implementations
  * Also, it is good for x64 and for x86, so, it seems, there is no sense to do 2 different realizations.
- * Reference speed: Hardware: 20GB/s, Software Native: 2GB/s, this: 850MB/s
+ * Reference speed: Hardware: 20GB/s, Software Native: 2GB/s, this: 1GB/s
  * 
  * Max Vysokikh, 2016
  */
@@ -16,13 +16,14 @@ namespace Crc32C
 
 		static SafeProxy()
 		{
+			uint[] table = _table;
 			for (uint i = 0; i < 256; i++)
 			{
 				uint res = i;
 				for (int t = 0; t < 16; t++)
 				{
 					for (int k = 0; k < 8; k++) res = (res & 1) == 1 ? Poly ^ (res >> 1) : (res >> 1);
-					_table[(t * 256) + i] = res;
+					table[(t * 256) + i] = res;
 				}
 			}
 		}
@@ -31,30 +32,31 @@ namespace Crc32C
 		{
 			uint crcLocal = uint.MaxValue ^ crc;
 
+			uint[] table = _table;
 			while (length >= 16)
 			{
-				crcLocal = _table[(15 * 256) + ((crcLocal ^ input[offset]) & 0xff)]
-					^ _table[(14 * 256) + (((crcLocal >> 8) ^ input[offset + 1]) & 0xff)]
-					^ _table[(13 * 256) + (((crcLocal >> 16) ^ input[offset + 2]) & 0xff)]
-					^ _table[(12 * 256) + (((crcLocal >> 24) ^ input[offset + 3]) & 0xff)]
-					^ _table[(11 * 256) + input[offset + 4]]
-					^ _table[(10 * 256) + input[offset + 5]]
-					^ _table[(9 * 256) + input[offset + 6]]
-					^ _table[(8 * 256) + input[offset + 7]]
-					^ _table[(7 * 256) + input[offset + 8]]
-					^ _table[(6 * 256) + input[offset + 9]]
-					^ _table[(5 * 256) + input[offset + 10]]
-					^ _table[(4 * 256) + input[offset + 11]]
-					^ _table[(3 * 256) + input[offset + 12]]
-					^ _table[(2 * 256) + input[offset + 13]]
-					^ _table[(1 * 256) + input[offset + 14]]
-					^ _table[(0 * 256) + input[offset + 15]];
+				crcLocal = table[(15 * 256) + ((crcLocal ^ input[offset]) & 0xff)]
+					^ table[(14 * 256) + (((crcLocal >> 8) ^ input[offset + 1]) & 0xff)]
+					^ table[(13 * 256) + (((crcLocal >> 16) ^ input[offset + 2]) & 0xff)]
+					^ table[(12 * 256) + (((crcLocal >> 24) ^ input[offset + 3]) & 0xff)]
+					^ table[(11 * 256) + input[offset + 4]]
+					^ table[(10 * 256) + input[offset + 5]]
+					^ table[(9 * 256) + input[offset + 6]]
+					^ table[(8 * 256) + input[offset + 7]]
+					^ table[(7 * 256) + input[offset + 8]]
+					^ table[(6 * 256) + input[offset + 9]]
+					^ table[(5 * 256) + input[offset + 10]]
+					^ table[(4 * 256) + input[offset + 11]]
+					^ table[(3 * 256) + input[offset + 12]]
+					^ table[(2 * 256) + input[offset + 13]]
+					^ table[(1 * 256) + input[offset + 14]]
+					^ table[(0 * 256) + input[offset + 15]];
 				offset += 16;
 				length -= 16;
 			}
 
 			while (--length >= 0)
-				crcLocal = _table[(crcLocal ^ input[offset++]) & 0xff] ^ crcLocal >> 8;
+				crcLocal = table[(crcLocal ^ input[offset++]) & 0xff] ^ crcLocal >> 8;
 			return crcLocal ^ uint.MaxValue;
 		}
 	}
